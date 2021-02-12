c11accf5468e423e2c04684bac6bed434788bccd
OMID <ahmadpooromid@gmail.com>
2021-02-12 19:05:36 +0330

    use FluentValidation for validating api endpoint request.

---
+++ src/Api/Controllers/v1/Products/Validators/AddProductRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using Api.Controllers.v1.Products.Requests;
+using FluentValidation;
+
+namespace Api.Controllers.v1.Products.Validators
+{
+    public class AddProductRequestValidator : AbstractValidator<AddProductRequest>
+    {
+        public AddProductRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull().NotEmpty().WithMessage("Name is not valid");
+
+            RuleFor(x => x.Price)
+                .NotNull().NotEmpty().GreaterThan(0).WithMessage("Price is not valid");
+        }
+    }
+}
