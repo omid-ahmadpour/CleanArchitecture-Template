ececdcfeef53a1ab30b78778784cd7b922db422e
Omid <Omid@DESKTOP-AJ6GC97>
2021-02-16 16:13:01 +0330

    ValidateModelStateAttribute for validating api calling request and return fluent validator messages.

---
+++ src/Web/ApiFramework/Attributes/ValidateModelStateAttribute.cs
@@ -0,0 +1,26 @@
+﻿using ApiFramework.Tools;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ApiFramework.Attributes
+{
+    public class ValidateModelStateAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                var modelStateEntries = context.ModelState.Values;
+
+                foreach (var item in modelStateEntries)
+                {
+                    foreach (var error in item.Errors)
+                    {
+                        ApiResult resultObject = new ApiResult(error.ErrorMessage);
+                        context.Result = new JsonResult(resultObject);
+                    }
+                }
+            }
+        }
+    }
+}
