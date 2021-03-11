af2ea2df221d500e363d4a446f1add02e02d3f75
OMID <ahmadpooromid@gmail.com>
2021-03-11 11:59:34 +0330

    remove unnecessary comments.

--- src/Common/DependencyInjection.cs
+++ src/Common/DependencyInjection.cs
@@ -7,13 +7,6 @@
     {
         public static IServiceCollection AddCommon(this IServiceCollection services, IConfiguration configuration)
         {
-            // services.AddAutoMapper(Assembly.GetExecutingAssembly());
-            // services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-            // services.AddMediatR(Assembly.GetExecutingAssembly());
-            // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
-            // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
-            // services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
-
             return services;
         }
     }
