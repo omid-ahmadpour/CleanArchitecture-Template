7188e5999479b9eb99bd3db2b6661b23ffe1df6d
OMID <ahmadpooromid@gmail.com>
2021-04-17 23:45:58 +0430

    add xUnit and Moq. add unit test for GetByIdAsync endpoint.

---
+++ test/Api.Test/ProductControllerTest.cs
@@ -0,0 +1,50 @@
+using Api.Controllers.v1.Products;
+using ApiFramework.Tools;
+using Application.Products.Query.GetProductById;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Api.Test
+{
+    public class ProductControllerTest
+    {
+        ProductController productController;
+        IMediator mediator;
+        ILogger<ProductController> logger;
+
+        public ProductControllerTest()
+        {
+            mediator = new Mock<IMediator>().Object;
+            logger = new Mock<ILogger<ProductController>>().Object;
+            productController = new ProductController(logger, mediator);
+        }
+
+        [Theory]
+        [InlineData(1,20)]
+        public async Task GetByIdAsyncTest(int id1,int id2)
+        {
+            //arrange
+            var validId = id1;
+            var invalidId = id2;
+
+            //act
+            var result = await productController.GetByIdAsync(validId);
+            var nullResult = await productController.GetByIdAsync(invalidId);
+
+            //assert
+            Assert.IsType<ApiResult<ProductQueryModel>>(result);
+
+            var nullItem = nullResult.Data;
+            Assert.Null(nullItem);
+
+            //var item = result.Data as ProductQueryModel;
+            //Assert.IsType<ProductQueryModel>(item);
+
+            //var productItem = item as ProductQueryModel;
+            //Assert.Equal(validId, productItem.ProductId);
+        }
+    }
+}
