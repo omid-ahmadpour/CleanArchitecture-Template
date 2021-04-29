7a9e1faf2f72a79cc7532944fa39857b1cfaf965
omid ahmadpour <sarzamin hoshmand@DESKTOP-7UHE6CV>
2021-04-29 14:41:44 +0430

    add some useful string extensions.

---
+++ src/Common/Utilities/StringExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Common.Utilities
+{
+    public static class StringExtensions
+    {
+        public static bool HasValue(this string value, bool ignoreWhiteSpace = true)
+        {
+            return ignoreWhiteSpace ? !string.IsNullOrWhiteSpace(value) : !string.IsNullOrEmpty(value);
+        }
+
+        public static int ToInt(this string value)
+        {
+            return Convert.ToInt32(value);
+        }
+
+        public static decimal ToDecimal(this string value)
+        {
+            return Convert.ToDecimal(value);
+        }
+
+        public static string ToNumeric(this int value)
+        {
+            return value.ToString("N0"); //"123,456"
+        }
+
+        public static string ToNumeric(this decimal value)
+        {
+            return value.ToString("N0");
+        }
+
+        public static string ToCurrency(this int value)
+        {
+            //fa-IR => current culture currency symbol => ریال
+            //123456 => "123,123ریال"
+            return value.ToString("C0");
+        }
+
+        public static string ToCurrency(this decimal value)
+        {
+            return value.ToString("C0");
+        }
+
+        public static string NullIfEmpty(this string str)
+        {
+            return str?.Length == 0 ? null : str;
+        }
+    }
+}
