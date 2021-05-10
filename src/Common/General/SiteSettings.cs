8c1f09b69170b20f5c9720491643e5aceef3dce6
omid ahmadpour <sarzamin hoshmand@DESKTOP-7UHE6CV>
2021-05-10 12:14:21 +0430

    add jwt setting.

---
+++ src/Common/General/SiteSettings.cs
@@ -0,0 +1,28 @@
+﻿namespace Common.General
+{
+    public class SiteSettings
+    {
+        public JwtSettings JwtSettings { get; set; }
+        public IdentitySettings IdentitySettings { get; set; }
+    }
+
+    public class IdentitySettings
+    {
+        public bool PasswordRequireDigit { get; set; }
+        public int PasswordRequiredLength { get; set; }
+        public bool PasswordRequireNonAlphanumeric { get; set; }
+        public bool PasswordRequireUppercase { get; set; }
+        public bool PasswordRequireLowercase { get; set; }
+        public bool RequireUniqueEmail { get; set; }
+    }
+
+    public class JwtSettings
+    {
+        public string SecretKey { get; set; }
+        public string EncryptKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int NotBeforeMinutes { get; set; }
+        public int ExpirationMinutes { get; set; }
+    }
+}
