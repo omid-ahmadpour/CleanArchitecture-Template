7460f211d77dd92fe47db17198767823fadfc62e
omid ahmadpour <sarzamin hoshmand@DESKTOP-7UHE6CV>
2021-05-10 12:19:30 +0430

    role and user fluent api configurations.

---
+++ src/Infrastructure/Persistance/Configuration/Users/RoleConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.dbo.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistance.Configuration.Users
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+        }
+    }
+}
---
+++ src/Infrastructure/Persistance/Configuration/Users/UserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities.dbo.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Persistance.Configuration.Users
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(p => p.UserName).IsRequired().HasMaxLength(100);
+        }
+    }
+}
