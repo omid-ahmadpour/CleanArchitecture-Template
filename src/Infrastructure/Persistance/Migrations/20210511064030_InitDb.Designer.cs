fa2698ca3a70529ffd8d7f5abfcfc8565eb90500
omid ahmadpour <sarzamin hoshmand@DESKTOP-7UHE6CV>
2021-05-11 11:15:28 +0430

    remove previous migration and add a new migration for all entities.

--- src/Infrastructure/Persistance/Migrations/20210511064030_InitDb.Designer.cs
+++ src/Infrastructure/Persistance/Migrations/20210511064030_InitDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistance.Db;
 
 namespace Persistance.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210511064030_InitDb")]
+    partial class InitDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
