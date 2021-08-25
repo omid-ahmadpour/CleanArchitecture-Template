cba8732d0ff80522b8a6c928348656d865298a74
omid <ahmadpooromid@gmail.com>
2021-08-25 15:13:24 +0430

    set validator for login and register apis.

---
+++ src/Web/Api/Controllers/v1/Users/Validators/LoginRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using Api.Controllers.v1.Users.Requests;
+using FluentValidation;
+
+namespace Api.Controllers.v1.Users.Validators
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Username)
+                .NotNull().NotEmpty().WithMessage("{PropertyName} is not valid");
+
+            RuleFor(x => x.Password)
+                .NotNull().NotEmpty().WithMessage("{PropertyName} is not valid");
+        }
+    }
+}
---
+++ src/Web/Api/Controllers/v1/Users/Validators/SingUpRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using Api.Controllers.v1.Users.Requests;
+using FluentValidation;
+
+namespace Api.Controllers.v1.Users.Validators
+{
+    public class SingUpRequestValidator : AbstractValidator<SingUpRequest>
+    {
+        public SingUpRequestValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotNull().NotEmpty().WithMessage("{PropertyName} is not valid");
+
+            RuleFor(x => x.Email)
+                .NotNull().NotEmpty().WithMessage("{PropertyName} is not valid");
+
+            RuleFor(x => x.Password)
+                .NotNull().NotEmpty().WithMessage("{PropertyName} is not valid");
+
+            RuleFor(x => x.FullName)
+                .NotNull().NotEmpty().WithMessage("{PropertyName} is not valid");
+
+            RuleFor(x => x.Age)
+                .NotNull().NotEmpty().GreaterThan(0).WithMessage("{PropertyName} is not valid");
+
+            RuleFor(x => x.Gender)
+                .NotNull().NotEmpty().WithMessage("{PropertyName} is not valid");
+        }
+    }
+}
