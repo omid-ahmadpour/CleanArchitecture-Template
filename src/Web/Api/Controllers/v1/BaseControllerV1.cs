070c0d96a337b59d89b4d878c0f18d3e6cc72260
omid <ahmadpooromid@gmail.com>
2021-09-08 21:16:02 +0430

    put in right place for each version.

---
+++ src/Web/Api/Controllers/v1/BaseControllerV1.cs
@@ -0,0 +1,26 @@
+﻿namespace Api.Controllers.v1
+{
+    using ApiFramework.Attributes;
+    using AutoMapper;
+    using MediatR;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+
+    [ValidateModelState]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class BaseControllerV1 : ControllerBase
+    {
+        internal readonly IMediator _mediator;
+        internal readonly IMapper _mapper;
+        internal readonly ILogger _logger;
+
+        protected BaseControllerV1(ILogger logger,
+                                   IMediator mediator,
+                                   IMapper mapper)
+        {
+            _logger = logger ?? throw new System.ArgumentNullException(nameof(logger));
+            _mediator = mediator ?? throw new System.ArgumentNullException(nameof(mediator));
+            _mapper = mapper ?? throw new System.ArgumentNullException(nameof(mapper));
+        }
+    }
+}
