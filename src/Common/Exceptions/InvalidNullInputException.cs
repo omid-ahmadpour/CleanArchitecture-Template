631a4c6aaea8b4c1ae34086bd995289f361126ff
omid <ahmadpooromid@gmail.com>
2022-01-21 13:38:02 +0330

    add a specific exception.

---
+++ src/Common/Exceptions/InvalidNullInputException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CleanTemplate.Common.Exceptions
+{
+    public class InvalidNullInputException : Exception
+    {
+        public InvalidNullInputException()
+            : base()
+        {
+        }
+
+        public InvalidNullInputException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidNullInputException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public InvalidNullInputException(string name, object key)
+            : base($"Input \"{name}\" ({key}) is not valid.")
+        {
+        }
+    }
+}
