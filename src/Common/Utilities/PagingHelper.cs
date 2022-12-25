7bf41eb0f319994bf7b7e94c544dd327523ae16b
omid ahmadpour <ahmadpooromid@gmail.com>
2022-12-25 22:24:36 +0100

    add a paging helper.

---
+++ src/Common/Utilities/PagingHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanTemplate.Common.Utilities
+{
+    public static class PagingHelper
+    {
+        public static async Task<PagedResult<T>> GetPaged<T>(this IQueryable<T> query,
+            int page, int pageSize) where T : class
+        {
+            var result = new PagedResult<T>
+            {
+                CurrentPage = page,
+                PageSize = pageSize,
+                RowCount = query.Count()
+            };
+
+            var pageCount = (double)result.RowCount / pageSize;
+            result.PageCount = (int)Math.Ceiling(pageCount);
+
+            var skip = (page - 1) * pageSize;
+            result.Results = await query.Skip(skip).Take(pageSize).ToListAsync();
+
+            return result;
+        }
+    }
+
+    public abstract class PagedResultBase
+    {
+        public int CurrentPage { get; set; }
+        public int PageCount { get; set; }
+        public int PageSize { get; set; }
+        public int RowCount { get; set; }
+
+        public int FirstRowOnPage => (CurrentPage - 1) * PageSize + 1;
+
+        public int LastRowOnPage => Math.Min(CurrentPage * PageSize, RowCount);
+    }
+
+    public class PagedResult<T> : PagedResultBase where T : class
+    {
+        public IList<T> Results { get; set; }
+
+        public PagedResult()
+        {
+            Results = new List<T>();
+        }
+    }
+}
